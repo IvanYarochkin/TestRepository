b8a0e5884a732da77ed1c0acf9a98d5f5929779a
Ivan Yarochkin <Ivan.Yarochkin97@gmail.com>
2019-05-22 01:47:22 +0300

    Implement a user entity

---
+++ src/main/java/com/epam/test/domain/User.java
@@ -0,0 +1,42 @@
+package com.epam.test.domain;
+
+import java.util.Objects;
+
+public class User extends Object{
+    private long id;
+
+    public User() {
+    }
+
+    public User(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                '}';
+    }
+}
